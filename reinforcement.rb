298b62b97db458b6c2700d0db748d1975f6ef2cf
YUMEE <yumeehk@hotmail.com>
2018-06-16 17:31:38 -0400

    finished assignment but not iterating through all of the variables

---
+++ reinforcement.rb
@@ -0,0 +1,25 @@
+seats = [
+  [nil, "Pumpkin", nil, nil],
+  ["Socks", nil, "Mimi", nil],
+  ["Gismo", "Shadow", nil, nil],
+  ["Smokey","Toast","Pacha","Mau"]
+]
+
+def seating(seats)
+  seats.each_with_index do |value, index|
+    value.each_with_index do |v, i|
+      if v == nil
+        puts "Row #{index +1} seat #{i +1} is free. Do you want to sit there? (y/n)"
+        user_says = gets.chomp
+        if user_says == "y"
+          puts "What is your name?"
+          name = gets.chomp
+        end
+      seats[index][i] = name
+      break
+      end
+    end
+  end
+end
+
+# not iterating through all the items in the list
